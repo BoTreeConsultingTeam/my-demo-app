0ceeea97e5f14570ab11d63c3b81b59b73dd513b
Suraj_95 <kostisuraj95@gmail.com>
2017-03-24 14:22:34 +0530

    view issue solved

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ActiveRecord::Base
 
   belongs_to :cleaner
-  belongs_to :city
+  belongs_to :city, class_name: "City", foreign_key: "customer_city_id"
   belongs_to :customer
 
   validates :customer_city_id, :presence => true
